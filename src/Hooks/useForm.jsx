b2dddc9025e888ca64693cde1d5924555471b232
Matheus Brito ££ <matheus.alvesdb@gmail.com>
2023-05-30 15:03:56 -0300

    Criada página de cadastro

--- src/Hooks/useForm.jsx
+++ src/Hooks/useForm.jsx
@@ -6,6 +6,11 @@ const types = {
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: 'Preencha um email válido',
   },
+  password: {
+    regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+    message:
+      'A senha deve conter no minímo 8 caracteres incluindo letra minúscula, maiúscula e digitos.',
+  },
 };
 
 const useForm = (type) => {
